c7148380878c651bda2a74ee1648be013d678622
healthier-vitamins <88876449+healthier-vitamins@users.noreply.github.com>
2022-07-14 16:39:46 +0800

    login done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,60 @@
+import { useAtom } from "jotai";
+import { useState } from "react";
+import { userAtom } from "../App";
+
 function Login() {
+    const [login, setLogin] = useAtom(userAtom);
+    const [form, setForm] = useState({
+        "email": "",
+        "password": ""
+    })
+
+    const handleChange = (e) => {
+        const {name, value} = e.target
+        setForm({
+            ...form,
+            [name]: value
+        })
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        fetch("https://workflow-management-backend.herokuapp.com/login-user", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(form)
+    })
+    .then((response) => response.json())
+    .then((data) => setLogin(data))
+    }
+
     return (
         <>
         <p>Login</p>
+        <form onSubmit={(e) => e.preventDefault()}>
+
+                <label htmlFor="email">Email</label>
+                <input 
+                    id="email"
+                    name="email"
+                    type="email"
+                    required
+                    value={form.email}
+                    onChange={handleChange}
+                />
+                <label htmlFor="password">Password</label>
+                <input 
+                    id="password"
+                    name="password"
+                    type="password"
+                    required
+                    value={form.password}
+                    onChange={handleChange}
+                />
+                <button onClick={handleSubmit}>Login</button>
+        </form>
         </>
     )
 }
