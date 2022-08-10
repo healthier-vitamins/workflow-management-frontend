befc6c7c0fa0621296e1d82fbfc20093e586ff18
healthier-vitamins <88876449+healthier-vitamins@users.noreply.github.com>
2022-08-11 03:13:56 +0800

    added ag-grid

--- src/pages/StockList.jsx
+++ src/pages/StockList.jsx
@@ -1,27 +1,55 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import Stock from "../components/StockList/Stock";
+import { AgGridReact } from "ag-grid-react";
+
+import "ag-grid-community/styles/ag-grid.css"; // Core grid CSS, always needed
+import "ag-grid-community/styles/ag-theme-alpine.css"; // Optional theme CSS
 
 function StockList() {
-  const [stockList, setStockList] = useState(null);
+  // const [stockList, setStockList] = useState(null);
+  const [rowData, setRowData] = useState();
+  const [columnDefs, setColumnDefs] = useState([
+    { field: "id", filter: true },
+    { field: "item", filter: true },
+    { field: "quantity", filter: true },
+  ]);
 
   useEffect(() => {
     fetch("https://workflow-management-backend.herokuapp.com/stock-list")
       .then((response) => response.json())
       .then((data) => {
-        setStockList(data);
+        setRowData(data);
         console.log(data);
       });
   }, []);
 
-  function ShowStocks() {
-    return stockList.map((ele, index) => <Stock stock={ele} key={index} />);
-  }
+  // DefaultColDef sets props common to all Columns
+  const defaultColDef = useMemo(() => ({
+    sortable: true,
+  }));
+
+  // function ShowStocks() {
+  //   return stockList.map((ele, index) => <Stock stock={ele} key={index} />);
+  // }
 
   return (
-    <div className="stock-list-page">
-      <h2>Stock List</h2>
-      <div className="stock-list-container">
-        {stockList === null ? <p>Loading</p> : <ShowStocks />}
+    // <div className="stock-list-page">
+    //   <h2>Stock List</h2>
+    //   <div className="stock-list-container">
+    //     {stockList === null ? <p>Loading</p> : <ShowStocks />}
+    //   </div>
+    // </div>
+    <div className="ag-grid-container">
+      <div
+        className="ag-theme-alpine"
+        style={{ width: "55em", height: "30em" }}
+      >
+        <AgGridReact
+          rowData={rowData} // Row Data for Rows
+          columnDefs={columnDefs} // Column Defs for Columns
+          defaultColDef={defaultColDef} // Default Column Properties
+          rowSelection="multiple" // Options - allows click selection of rows
+        />
       </div>
     </div>
   );
